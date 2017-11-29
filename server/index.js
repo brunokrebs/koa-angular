1ed5679114456471f8c8710547e2c13b5ea00108
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-29 15:31:55 -0200

    securing backend with Auth0

--- server/index.js
+++ server/index.js
@@ -2,9 +2,23 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const mongo = require('./mongo');
+const jwt = require('koa-jwt');
+const {koaJwtSecret} = require('jwks-rsa');
+const cors = require('kcors');
 
 const app = new Koa();
 const router = new Router();
+
+app.use(cors());
+
+app.use(jwt({
+  secret: koaJwtSecret({
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+    cache: true
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`
+}));
 
 router.get('/:entity', async function (ctx) {
   const {entity} = ctx.params;
